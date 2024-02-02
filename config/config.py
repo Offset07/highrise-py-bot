81f0b0745e1a7c8789d12edd11fad2fe6cceef14
Offset07 <124641446+Offset07@users.noreply.github.com>
2024-02-02 13:48:48 +0000

    config.py

--- config/config.py
+++ config/config.py
@@ -1,10 +1,10 @@
 class config:
     # Basic configuration: If you are unsure how to obtain the Bot ID, simply start the bot and it will be logged in the console.
     prefix = '/'
-    botID = 'change-me'
-    botName = 'change-me'
-    ownerName = 'change-me'
-    roomName = 'change-me'
+    botID = '01b50864d754b0b8522967751d2b34d252b2e8bcc065ef7327b70dfb9d14aaa6'
+    botName = 'offsetbot'
+    ownerName = 'daniel_offset'
+    roomName = 'bestie'
     coordinates = {
         'x': 8.5,
         'y': 0.6000,
@@ -21,7 +21,7 @@ class loggers:
     joins = True
     leave = True
     tips = True
-    emotes = False
+    emotes = True
     reactions = False
     userMovement = False
 
@@ -37,12 +37,12 @@ class messages:
 
 class permissions:
     # You can add as many IDs as you want, for example: ['id1', 'id2'].
-    owners = ['55bb64735531104341039ca8']
+    owners = ['daniel_offset']
     moderators = ['55bb64735531104341039ca8']
 
 
 class authorization:
     # To obtain your token, visit https://highrise.game/ and log in. Then, go to the settings and create a new bot. Accept the terms and generate a token.
     # To obtain your room ID, go to the game and navigate to the top right corner where the player list is displayed. Click on "Share this room" and copy the ID.
-    room = 'change-me'
-    token = 'change-me'
+    room = '64e99ff81188da64f8889bfa'
+    token = '01b50864d754b0b8522967751d2b34d252b2e8bcc065ef7327b70dfb9d14aaa6'
